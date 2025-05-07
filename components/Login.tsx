28c6b8366acf55048c2fd27690ffe08cd5c7318e
Janvi Ahuja <janviahuja@Janvis-MacBook-Air.local>
2025-05-06 21:13:07 -0400

    Use env API URL for signup route

--- components/Login.tsx
+++ components/Login.tsx
@@ -10,7 +10,7 @@ export default function Login({ onLoginSuccess }: LoginProps) {
 
   const handleLogin = async () => {
     try {
-      const res = await fetch('http://localhost:5050/api/auth/login', {
+      const res = await fetch('${process.env.NEXT_PUBLIC_API_URL}/api/auth/Login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
--- components/Signup.tsx
+++ components/Signup.tsx
@@ -11,7 +11,7 @@ export default function Signup({ onSignupSuccess }: SignupProps) {
 
   const handleSignup = async () => {
     try {
-      const res = await fetch('http://localhost:5050/api/auth/signup', {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/signup`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
