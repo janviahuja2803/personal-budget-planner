5b30ae8b929ca7a6e1fe1da5460c6dceb2c940c0
Janvi Ahuja <janviahuja@Janvis-MacBook-Air.local>
2025-05-06 21:28:06 -0400

    Final chmages

--- components/Login.tsx
+++ components/Login.tsx
@@ -10,7 +10,7 @@ export default function Login({ onLoginSuccess }: LoginProps) {
 
   const handleLogin = async () => {
     try {
-      const res = await fetch('${process.env.NEXT_PUBLIC_API_URL}/api/auth/Login', {
+      const res = await fetch('${process.env.NEXT_PUBLIC_API_URL}/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
