7fdd00310a6b00d9980fc1aa12f305d770d90da9
Janvi Ahuja <janviahuja@Janvis-MacBook-Air.local>
2025-05-06 20:43:18 -0400

    Fix case in import path for Signup.tsx

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,4 +1,4 @@
-import Signup from '../components/SignUp';
+import Signup from '../components/Signup';
 
 export default function SignupPage() {
   return <Signup onSignupSuccess={() => window.location.href = '/dashboard'} />;
